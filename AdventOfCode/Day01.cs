c3771fcf36c0027ec8607f06e4516a8396a9f56f
Stevie Robinson <stevie.robinson@gmail.com>
2025-12-06 12:04:07 +0100

    Day 01 solutions

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -2,14 +2,66 @@
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly List<string> _input;
+    private int _position = 50;
+    private int _zeroCount = 0;
+    private int _timesPastZero = 0;
+        
+
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllLines(InputFilePath).ToList();
+        ProcessInput();
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    private void ProcessInput()
+    {
+        foreach (var line in _input)
+        {
+            var direction = line[0];
+            var turns = int.Parse(line[1..]);
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+            if (direction == 'R')
+            {
+                for (var i = 0; i < turns; i++)
+                {
+                    _position++;
+
+                    if (_position == 100)
+                    {
+                        _position = 0;
+                        _timesPastZero++;
+                    }
+                }
+            }
+
+            if (direction == 'L')
+            {
+                for (var i = 0; i < turns; i++)
+                {
+                    _position--;
+
+                    if (_position == 0)
+                    {
+                        _timesPastZero++;
+                    }
+
+                    if (_position == -1)
+                    {
+                        _position = 99;
+                    }
+                }
+            }
+
+            if (_position == 0)
+            {
+                _zeroCount++;
+            }
+        }
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{_zeroCount}");
+
+    public override ValueTask<string> Solve_2() => new($"{_timesPastZero}");
 }
